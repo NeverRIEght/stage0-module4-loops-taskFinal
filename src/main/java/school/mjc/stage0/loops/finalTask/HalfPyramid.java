8fa450dcb0750994ec73384fcb4dfa7067f7a0a9
NeverRIEght <lfkomarovme@gmail.com>
2023-07-09 16:49:12 +0300

    Half-Pyramid

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for(int i = 1; i <= cathetusLength; i++) {
+            for(int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = cathetusLength - i; k < cathetusLength; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 
     public static void main(String[] args) {
